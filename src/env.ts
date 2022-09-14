4a25bb59306b267fba776fc73e7d2232c33e151b
NilsOveTen <github.nils.ove@tendenes.com>
2022-09-14 08:17:19 +0200

    chore: use updated org-cat uri

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,7 @@ interface EnvironmentVariables {
   OIDC_ISSUER: string;
   DATA_SERVICE_CATALOG_URL: string;
   FDK_REGISTRATION_BASE_URI: string;
-  ORGANIZATION_API: string;
+  ORGANIZATION_CATALOG_URI: string;
   SEARCH_API: string;
   SEARCH_FULLTEXT_HOST: string;
   ADMIN_GUI_HOST: string;
@@ -18,8 +18,8 @@ const env = ((window as any).env || {
     'https://dataservice-catalog.staging.fellesdatakatalog.digdir.no',
   FDK_REGISTRATION_BASE_URI:
     'https://registrering.staging.fellesdatakatalog.digdir.no',
-  ORGANIZATION_API:
-    'https://organization-catalogue.staging.fellesdatakatalog.digdir.no',
+  ORGANIZATION_CATALOG_URI:
+    'https://organization-catalog.staging.fellesdatakatalog.digdir.no',
   SEARCH_API: 'https://staging.fellesdatakatalog.digdir.no',
   SEARCH_FULLTEXT_HOST: 'https://search.staging.fellesdatakatalog.digdir.no',
   ADMIN_GUI_HOST: 'https://admin.fellesdatakatalog.digdir.no',
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,7 +2,7 @@ export interface Configuration {
   OIDC_ISSUER: string;
   DATA_SERVICE_CATALOG_URL: string;
   FDK_REGISTRATION_BASE_URI: string;
-  ORGANIZATION_API: string;
+  ORGANIZATION_CATALOG_URI: string;
   SEARCH_API: string;
   SEARCH_FULLTEXT_HOST: string;
   ADMIN_GUI_HOST: string;
