311bb1590a5b526dfbc86ffae815f13ca5e87fec
NilsOveTen <github.nils.ove@tendenes.com>
2022-09-23 09:19:30 +0200

    fix: use registration portal as post logout uri

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,14 +1,14 @@
 import { Auth } from '../lib/auth/auth';
 import env from '../env';
 
-const { OIDC_ISSUER } = env;
+const { FDK_REGISTRATION_BASE_URI, OIDC_ISSUER } = env;
 const OIDC_CLIENT_ID = 'dataservice-catalog-gui';
 
 export const authService = new Auth({
   oidcIssuer: OIDC_ISSUER,
   clientId: OIDC_CLIENT_ID,
   redirectUri: location.href,
-  logoutRedirectUri: location.origin,
+  logoutRedirectUri: FDK_REGISTRATION_BASE_URI,
   silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`
 });
 
