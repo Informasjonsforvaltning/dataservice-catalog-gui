270473b597fc35706c4aaab0cf739ba4870ab04d
NilsOveTen <github.nils.ove@tendenes.com>
2023-06-30 15:18:53 +0200

    fix: handle org write role

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -36,21 +36,18 @@ export class Auth {
     this.kc = Keycloak(kcConfig);
   }
 
-  init: ({
-    loginRequired
-  }: {
-    loginRequired: boolean;
-  }) => Promise<boolean> = async ({ loginRequired }) => {
-    const keycloakInitOptions: KeycloakInitOptions = {
-      onLoad: 'check-sso',
-      silentCheckSsoRedirectUri: this.conf.silentCheckSsoRedirectUri
+  init: ({ loginRequired }: { loginRequired: boolean }) => Promise<boolean> =
+    async ({ loginRequired }) => {
+      const keycloakInitOptions: KeycloakInitOptions = {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri: this.conf.silentCheckSsoRedirectUri
+      };
+      await this.kc.init(keycloakInitOptions).catch(console.error);
+      if (loginRequired && !this.isAuthenticated()) {
+        await this.login();
+      }
+      return this.isAuthenticated();
     };
-    await this.kc.init(keycloakInitOptions).catch(console.error);
-    if (loginRequired && !this.isAuthenticated()) {
-      await this.login();
-    }
-    return this.isAuthenticated();
-  };
 
   login: () => Promise<void> = () =>
     this.kc.login().then().catch(console.error);
@@ -115,7 +112,7 @@ export class Auth {
     );
 
   hasOrganizationWritePermission = (orgNr: string) =>
-    this.hasOrganizationRole({ orgNr, role: 'admin' });
+    this.hasOrganizationRole({ orgNr, role: 'write' });
 
   hasOrganizationAdminPermission = (orgNr: string) =>
     this.hasOrganizationRole({ orgNr, role: 'admin' });
